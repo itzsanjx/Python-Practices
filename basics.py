8fa63c13b0154332668ea90677ea55e1eb73b717
itzsanjx <sanjuaarthi01@gmail.com>
2026-02-05 17:32:13 +0530

    basics learnt from python

---
+++ basics.py
@@ -0,0 +1,79 @@
+
+# This is comment  when the hastage has been put in the code it will readed as a comment 
+
+"""
+this is the multi line comment used for 
+some explanation in the code whisch ahsmore explanaion
+
+"""
+ 
+# Case senstive 
+
+"""
+Case Sestive  ABC abc Abc
+
+"""
+# Key Words 
+
+"""
+reserved word for python eg : True False
+
+"""
+# Variables 
+
+"""
+variable that starts with alphabet or an underscore (abc, __)
+
+"""
+"""
+a = 10
+sanju = rthgf
+rea# = 45
+
+"""
+# Data Types
+"""
+Integer = 100 ( numbers are dentoed as number in the variable represents )
+Float = 10.3 ( when comes with decimal form its a float data types)
+Complex = 50j ( when j comes after the  number (j)= square root of -1)
+String = ("hi this price is 400$ )string is used for store any type of data inside the value of "" or '')
+boolean = key words reservedin python libraries
+
+"""
+
+#Print()-function
+"""
+two types of function in this sequence 
+that is print(a) that print the variabl that stored in the variable a 
+another one is print("a") actualy prints an a is the output.
+"""
+"""
+a = 10
+saanju = "name in it"
+print(a,saanju)
+print("defenition")
+""" 
+#  Escape Sequence 
+"""
+print("hi sanju \\'how are you \"iam fine about u\"") 
+
+
+print("hi da \tmapla \"epadi \niruka\"")
+"""
+#type casting 
+"""
+
+
+
+a=10
+b=10.5
+c=10+4j
+d="hisanju"
+e= False 
+print(a,type(a),b,type(b),c,type(c),d,type(d),e,type(e))
+"""
+total=0
+for i in range(1, 1000):
+    if i % 3 == 0 or i % 5 == 0:
+         total += i
+print("Sum of all multiples of 3 or 5 below 1000 is:", total)
